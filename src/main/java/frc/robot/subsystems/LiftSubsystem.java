3d4ee306e98575b98f161f70b882c082642fe976
judevalens <judevalens@gmail.com>
2019-03-17 14:29:23 -0400

    Update LiftSubsystem.java

--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -35,14 +35,6 @@ public class LiftSubsystem extends Subsystem {
   public TalonSRX liftTalonSlave;
   private PIDController liftPidController;
 
-  private boolean startRecordPeakOutput = false;
-  private ArrayList<Integer> velocities= new ArrayList<>();
-  private Timer recordPeakOutputTimer = new Timer();
-  private double deltaRecordPeakOutputTimer = 0.0;
-  private double RecordPeakOutputIterator = 0;
-  private int RecordPeakOutputMax = 0;
-
-
   public LiftSubsystem() {
 
     liftTalon = new TalonSRX(RobotMap.liftMotor);
@@ -172,26 +164,7 @@ public class LiftSubsystem extends Subsystem {
     return talonUnitsToInches(liftTalon.getSelectedSensorVelocity()) * 10;
   }
 
-  public void recordPeakOuput(){
-
-    if(!startRecordPeakOutput){
-      recordPeakOutputTimer.start();
-      startRecordPeakOutput = true;
-    }
-
-    if(recordPeakOutputTimer.get() <= 5){
-      if((recordPeakOutputTimer.get()-deltaRecordPeakOutputTimer) >= (0.05)){
-        deltaRecordPeakOutputTimer += recordPeakOutputTimer.get();
-        velocities.add(liftTalon.getSelectedSensorPosition());
-      }
-    }else{
-      while(RecordPeakOutputIterator < velocities.size()){
-          
-      }
-    }
-    
-  }
-
+  
   @Override
   public void initDefaultCommand() {
     setDefaultCommand(new LiftCommand());
