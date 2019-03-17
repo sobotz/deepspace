5f90276f0e198cb128707577b6ae3ad41a213633
judevalens <judevalens@gmail.com>
2019-03-17 14:38:24 -0400

    Update PurePursuitCommand.java

--- src/main/java/frc/robot/commands/PurePursuitCommand.java
+++ src/main/java/frc/robot/commands/PurePursuitCommand.java
@@ -12,6 +12,7 @@ import frc.robot.navigation.Controller;
 import frc.robot.navigation.Point;
 
 import java.util.HashMap;
+import edu.wpi.first.wpilibj.Timer;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -19,24 +20,26 @@ public class PurePursuitCommand extends Command {
   Point[] purePursuitPath;
   Controller purePursuit;
   private HashMap<String, Double> wV = new HashMap<>();
+  private Timer time;
 
   public PurePursuitCommand(Point[] path) {
     purePursuitPath = path;
     requires(Robot.m_drivesubsystem);
     purePursuit = new Controller(path, 0.8, 0.001);
+    time = new Timer();
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    time.start();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-
-    wV = purePursuit.controlLoop(Robot.m_drivesubsystem.talonUnitsToInches(Robot.m_drivesubsystem.frontLeftTalon), Robot.m_drivesubsystem.talonUnitsToInches(Robot.m_drivesubsystem.frontRightTalon), Robot.m_drivesubsystem.ahrs.getYaw(),Robot.m_drivesubsystem.talonVelocityToNormal(Robot.m_drivesubsystem.frontLeftTalon),Robot.m_drivesubsystem.talonVelocityToNormal(Robot.m_drivesubsystem.frontRightTalon));
-  
+    this.wV = purePursuit.controlLoop(Robot.m_drivesubsystem.talonUnitsToInches(Robot.m_drivesubsystem.frontLeftTalon), Robot.m_drivesubsystem.talonUnitsToInches(Robot.m_drivesubsystem.frontRightTalon), Robot.m_drivesubsystem.ahrs.getYaw(),Robot.m_drivesubsystem.talonVelocityUnitsToNormal(Robot.m_drivesubsystem.frontLeftTalon),Robot.m_drivesubsystem.talonVelocityUnitsToNormal(Robot.m_drivesubsystem.frontRightTalon), time.get());
+    System.out.print(wV.get("Left") + " " + wV.get("Right"));
   }
 
   // Make this return true when this Command no longer needs to run execute()
