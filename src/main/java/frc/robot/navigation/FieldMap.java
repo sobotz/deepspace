95f3522f9a35ea04317fb93cbc50462514090c84
Olivia Steger <ogsteger@gmail.com>
2019-08-14 12:37:28 -0400

    Linting FieldMap.java

--- src/main/java/frc/robot/navigation/FieldMap.java
+++ src/main/java/frc/robot/navigation/FieldMap.java
@@ -3,21 +3,25 @@ package frc.robot.navigation;
 public class FieldMap {
     private double width;
     private double height;
-   public FieldMap(double width, double height) {
-       this.width = width;
-       this.height = height;
-   }
-   public double getWidth() {
-       return width;
-   }
-   public void setWidth(double width) {
-       this.width = width;
-   }
-   public double getHeight() {
-       return height;
-   }
-   public void setHeight(double height) {
-       this.height = height;
-   }
     
-   }
+    public FieldMap(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    public double getWidth() {
+        return width;
+    }
+    
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+    
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
