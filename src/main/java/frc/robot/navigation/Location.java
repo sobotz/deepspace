bfe94885f9ced25c77961d23559e0dc3d9f8b9c2
Olivia Steger <ogsteger@gmail.com>
2019-08-14 12:38:22 -0400

    Linting Location.java

--- src/main/java/frc/robot/navigation/Location.java
+++ src/main/java/frc/robot/navigation/Location.java
@@ -4,42 +4,29 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class Location {
-	
-	private Point currentPosition;
-	private Encoder leftEncoder;
-	private Encoder rightEncoder;
-	private double xLocation;
-	private double yLocation;
-	private double distance;
-	private Gyro gyro;
-	
-	public Location(Encoder left, Encoder right, Gyro a) {
-		leftEncoder = left;
-		rightEncoder = right;
-		gyro = a;
-		
-		distance = Math.abs((6 * 3.14) * (rightEncoder.get() + leftEncoder.get() / 2) / 360);
-		xLocation = distance * Math.cos(gyro.getAngle());
-		yLocation = distance * Math.sin(gyro.getAngle());
-		currentPosition = new Point(xLocation, yLocation);
-	}
+    private Point currentPosition;
+    private Encoder leftEncoder;
+    private Encoder rightEncoder;
+    private double xLocation;
+    private double yLocation;
+    private double distance;
+    private Gyro gyro;
 
-	public Point getCurrentPosition() {
-		return currentPosition;
-	}
-	
-	public void setCurrentPosition(Point currentPosition) {
-		this.currentPosition = currentPosition;
-	}
+    public Location(Encoder left, Encoder right, Gyro a) {
+        leftEncoder = left;
+        rightEncoder = right;
+        gyro = a;
+        distance = Math.abs((6 * 3.14) * (rightEncoder.get() + leftEncoder.get() / 2) / 360);
+        xLocation = distance * Math.cos(gyro.getAngle());
+        yLocation = distance * Math.sin(gyro.getAngle());
+        currentPosition = new Point(xLocation, yLocation);
+    }
 
-	/*
-	public void move(double changeLeft, double changeRight, double angle) {
-		double distance = (changeLeft + changeRight)/2.0;
-		
-		double newX = distance * Math.cos(Math.toRadians(angle));
-		double newY = distance * Math.sin(Math.toRadians(angle));
-		currentPosition.moveX(newX - currentPosition.getX());
-		currentPosition.moveY(newY - currentPosition.getY());
-	}
-	*/
+    public Point getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Point currentPosition) {
+        this.currentPosition = currentPosition;
+    }
 }
